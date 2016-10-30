2b12e3df6cb0684227f2b48811ebfde19f26552a
Javier Peces <javierpeces@users.noreply.github.com>
2016-10-30 20:06:41 +0100

    Create 60-five-min-a-day-pythonic.py

---
+++ 60-five-min-a-day-pythonic.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+#
+# Looks like this is not pythonic:
+#
+
+my_container = ['Larry', 'Moe', 'Curly']
+index = 0
+for element in my_container:
+    print ('{} {}'.format(index, element))
+    index += 1
+    
+#
+# and this IS:
+#
+
+my_container = ['Larry', 'Moe', 'Curly']
+for index, element in enumerate(my_container):
+    print ('{} {}'.format(index, element))
+    
+#
+# source: https://jeffknupp.com/writing-idiomatic-python-ebook/
+#
