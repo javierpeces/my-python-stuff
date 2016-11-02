f7de79d9771a3815e5e46c8a81b9746842851220
Javier Peces <javierpeces@users.noreply.github.com>
2016-11-02 22:18:09 +0100

    Update 90-fmad-more-lists.py

--- 90-fmad-more-lists.py
+++ 90-fmad-more-lists.py
@@ -6,9 +6,26 @@
 mylist = ["canguro", 123.44176, "Ptolomeo", 100, 'more', 'language']
 
 mylist.append("otro")
-print(mylist)
+print("the original list adding 'otro' at the end....... {}".format(mylist))
 
-mylist.delete("more")
-print(mylist)
+mylist.remove("more")
+print("remove element that contains the string 'more'... {}".format(mylist))
+
+mylist.pop(3)
+print("Take away the element in place 3. First is 0..... {}".format(mylist))
+
+mylist.pop()
+print("Pop no args removes the last element............. {}".format(mylist))
+
+mylist.pop(-1)
+print("Pop with -1 removes the last element............. {}".format(mylist))
+
+# $ ./lists.py 
+# the original list adding 'otro' at the end....... ['canguro', 123.44176, 'Ptolomeo', 100, 'more', 'language', 'otro']
+# remove element that contains the string 'more'... ['canguro', 123.44176, 'Ptolomeo', 100, 'language', 'otro']
+# Take away the element in place 3. First is 0..... ['canguro', 123.44176, 'Ptolomeo', 'language', 'otro']
+# Pop no args removes the last element............. ['canguro', 123.44176, 'Ptolomeo', 'language']
+# Pop with -1 removes the last element............. ['canguro', 123.44176, 'Ptolomeo']
+# The End
 
 print("The End")
