4a1d74d4d332cb7d78cc3c94d41f27e1b4c9e818
Javier Peces <javierpeces@users.noreply.github.com>
2016-11-02 21:58:05 +0100

    Create 90-fmad-more-lists.py

---
+++ 90-fmad-more-lists.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#
+# Revisiting lists
+#
+
+mylist = ["canguro", 123.44176, "Ptolomeo", 100, 'more', 'language']
+
+mylist.append("otro")
+print(mylist)
+
+mylist.delete("more")
+print(mylist)
+
+print("The End")
