1b870436305ba59b26f1aa64011bbe2bcbb9cb74
Javier Peces <javierpeces@users.noreply.github.com>
2016-11-08 03:44:31 +0100

    Create 110-fmad-multiple-if.py

---
+++ 110-fmad-multiple-if.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# self explained multiple 'if elif else' structure
+# to make decisions based in ranges
+
+fortune = 97.2
+
+if fortune < 1:
+    print("so-o-o-o poor")
+elif fortune < 10:
+    print("simply poor")
+elif fortune < 100:
+    print("lo class")
+else:
+    print("got enough")
+
+print("the end")
