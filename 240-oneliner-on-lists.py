2efa2b924529f421951329fe0177e9b6e8990dd9
Javier Peces <javierpeces@users.noreply.github.com>
2018-11-07 01:41:04 +0100

    Create 240-oneliner-on-lists.py

---
+++ 240-oneliner-on-lists.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+
+  #
+  # Simplest oneliner
+  #
+
+  nlist = { 7, 14, 21 }
+  doble = [ num * 2 for num in nlist if num > 10 ]
+  print( doble )
+
+  #
+  # Classic loop moving across a list
+  #
+  
+  slist = { 0: "a", 1: "ante", 2: "bajo", 3: "cabe", 4: "con", 5: "contra", 6: "de", 7: "desde" }
+  for key, item in slist.items( ):
+    print( "The key {} has value {}".format( key, item ) )
+    
+  #
+  # Python loop doing the same
+  #
+  
+  dummy = [ print( "K {} V {}".format( key, val ) ) for key, val in slist.items( ) if val != "cabe" ]
+  
+  #
+  # That's all folks
+  #
